8a873822b8070785bd92329c2bc08d0feb9ba1ca
leewwe <leewwe@hrbeu.edu.cn>
2019-11-04 23:01:41 +0800

    167两数之和2

--- 167.two-sum-ii-input-array-is-sorted.cpp
+++ 167.two-sum-ii-input-array-is-sorted.cpp
@@ -39,8 +39,24 @@
 // @lc code=start
 class Solution {
 public:
+// 双指针法，官方解题https://leetcode-cn.com/problems/two-sum-ii-input-array-is-sorted/solution/liang-shu-zhi-he-ii-shu-ru-you-xu-shu-zu-by-leetco/
     vector<int> twoSum(vector<int>& numbers, int target) {
-        
+        int low = 0; 
+        int high = numbers.size() - 1;
+        while (low < high) {
+            int sum = numbers[low] + numbers[high];
+            if (sum == target) {
+                return { low + 1,high + 1 };
+            }
+            else if (sum > target) {
+                --high;
+            }
+            else {
+                ++low;
+            }
+        }
+
+        return { -1,-1 };
     }
 };
 // @lc code=end
